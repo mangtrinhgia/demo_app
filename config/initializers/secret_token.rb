0463070550fa8dc084356f8f6268f42f64a63e60
Nguyen Vinh Thinh <nvthinh.hut.hedspi@gmail.com>
2013-06-11 13:22:29 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3b57b33a84465fc3733140bbba224040fcc3806bee8ecdfcc73415611a79ca3f3c36edd0bea884b74e41e82c030adcb7a402c124585be8be368f6dd6db85cc54'
